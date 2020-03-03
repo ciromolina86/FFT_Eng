01ce91e3d94b873f3adecfe7ff45906f4103f818
Ciro Molina <ciromolina86@gmail.com>
2020-03-03 14:44:46 -0500

    testing read tdw and write to influx

--- databases_conn.py
+++ databases_conn.py
@@ -120,12 +120,13 @@ class DBinflux:
     def client(self):
         return self._client
 
-    def query(self, sql):
-        return self.client.query(sql)  #, epoch='ms'
+    def query(self, sql, bind_params={}):
+        return self.client.query(query=sql, bind_params=bind_params)
 
-    def read_tdw(self, asset_name='VIB_SENSOR1'):
+    def read_tdw(self, meas):
         # TODO
-        print('')
+        pdf = 0
+        return pdf
 
     def write_points(self, pdf, meas):
         self.client.write_points(dataframe=pdf, measurement=meas, time_precision='ms')
@@ -224,8 +225,85 @@ def test_influx():
     # print(pdf_all)
     print('ALL shape {}'.format(pdf_all.shape))
 
+def write_influx_test_data():
+    # create a testing wave
+    wave = thinkdsp.SinSignal(freq=10, amp=1, offset=0).make_wave(duration=1, start=0, framerate=100)
+
+    # create an influxdb client
+    client = InfluxDBClient(host='192.168.21.134', port=8086, database='VIB_DB')
+
+    # Generating point for WF___X_EVT_CHG_ID
+    points = [{
+        "measurement": 'VIB_SEN1',
+        "fields": {
+            "WF___X_EVT_CHG_ID": '2020-03-03 01:01:00.000000+00:00'
+        }
+    }]
+    client.write_points(points, time_precision='ms')
+
+    # Generating points for WF___X_TDW and WF___X_EVTID
+    for k in wave.ys:
+        points = [{
+            "measurement": 'VIB_SEN1',
+            "fields": {
+                "WF___X_TDW": k,
+                "WF___X_EVTID": '2020-03-03 01:01:00.000000+00:00'
+            }
+        }]
+        client.write_points(points, time_precision='ms')
 
 
+def read_influx_test_data():
+    # Initialization
+    DATABASE_NAME = 'VIB_DB'
+    ASSET_NAME = "VIB_SEN1"
+    _timestamp = '_timestamp'
+    X_EVTID = 'WF___X_EVTID'
+    X_EVT_CHG_ID = 'WF___X_EVT_CHG_ID'
+    X_FFT = 'WF___X_FFT'
+    X_FFT_RED = 'WF___X_FFT_RED'
+    X_FREQ = 'WF___X_FREQ'
+    X_FREQ_RED = 'WF___X_FREQ_RED'
+    X_TDW = 'WF___X_TDW'
+    X_TDW_RED = 'WF___X_TDW_RED'
+    Z_EVTID = 'WF___Z_EVTID'
+    Z_EVT_CHG_ID = 'WF___Z_EVT_CHG_ID'
+    Z_FFT = 'WF___Z_FFT'
+    Z_FFT_RED = 'WF___Z_FFT_RED'
+    Z_FREQ = 'WF___Z_FREQ'
+    Z_FREQ_RED = 'WF___Z_FREQ_RED'
+    Z_TDW = 'WF___Z_TDW'
+    Z_TDW_RED = 'WF___Z_TDW_RED'
+
+    X_EVTID1 = '2020-03-03 01:01:00.000000+00:00'
+
+    # define database configuratin parameters
+    db_info = {}
+    db_info.update({'host': "192.168.21.134"})  # localhost, 192.168.1.118
+    db_info.update({'port': 8086})
+    db_info.update({'database': DATABASE_NAME})
+
+    # create an instance of DBinflux
+    db1 = DBinflux(config=db_info)
+
+    # sql = "select * from " + ASSET_NAME
+    # sql = "select " + X_EVTID + " from " + ASSET_NAME
+    # sql = "select {}, {}, {} from {} order by time".format(X_TDW, X_EVTID, X_EVT_CHG_ID, ASSET_NAME)
+    sql = "select {}, {}, {} from {} where WF___X_EVTID=$X_EVTID1;".format(X_TDW, X_EVTID, X_EVT_CHG_ID, ASSET_NAME)
+    # sql = "select {} from {} where {}=$X_EVTID1;".format(X_EVT_CHG_ID, ASSET_NAME, X_EVTID)
+    bind_params = {'X_EVTID1': X_EVTID1}
+    # sql = "select $ts, $tdw, $eid, $eid_chg from $meas order by time".format(_timestamp, X_TDW, X_EVTID, X_EVT_CHG_ID, ASSET_NAME)
+    # bind_params = {'ts': _timestamp, 'tdw': X_TDW, 'eid': X_EVTID, 'eid_chg': X_EVT_CHG_ID, 'meas': ASSET_NAME}
+
+    # Execute query
+    datasets_dic = db1.query(sql, bind_params)
+
+    # Get pandas dataframe
+    pdf_wave = datasets_dic[ASSET_NAME]
+    # pdf_wave.to_csv(path_or_buf='C://Users//cmolina//Desktop//pdf_wave.csv')
+    print(pdf_wave)
+    # print('TDW shape: {}'.format(pdf_wave.shape))
+    # print(pdf_wave.keys)
 
 
 if __name__ == "__main__":
@@ -239,4 +317,6 @@ if __name__ == "__main__":
     # test_influxdb_conn.writeTestValues2()
 
     # read data from influx
-    test_influx()
+    # test_influx()
+    # write_influx_test_data()
+    read_influx_test_data()
